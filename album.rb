0f3fa552a522f048cac80f38906ea724e51f3076
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-22 18:52:01 +0000

    automatically backed up by learn

---
+++ album.rb
@@ -0,0 +1,18 @@
+class = Album
+  
+  def initialize(name,date)
+    @name=name
+    @date=date
+  end
+  
+  def name_of_album
+    @name
+  end
+  
+  def date_of_album
+    @date 
+  end
+
+end
+
+shawn_mendes_album = Album.new("Shawn Mendes", "2018")
