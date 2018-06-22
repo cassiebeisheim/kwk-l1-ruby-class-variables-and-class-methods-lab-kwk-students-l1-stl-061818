fd31527035684ed163f3b9042411736d6194153e
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-22 19:06:37 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -1,8 +1,14 @@
 class Album
+  @@album_count =0 
   
   def initialize(name,date)
     @name=name
     @date=date
+    @@album_count=@@album_count+1 
+  end
+  
+  def self.count 
+    @@album_count
   end
   
   def name_of_album
@@ -16,6 +22,10 @@ class Album
 end
 
 shawn_mendes_album = Album.new("Shawn Mendes", "2018")
+taylor_swift_album = Album.new("Reputation", "2017")
+drake_album = Album.new("Scorpion", "2018")
 
 
 puts "shawn mendes was made in #{shawn_mendes_album.date_of_album}"
+
+puts "#{album_count}"
