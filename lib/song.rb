417b404cf5904e3c37670a62e81aec932d1fea76
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-22 20:25:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,17 @@ def get_count
   @@count
 end 
 
+def self.get_count_other_way
+  @@count
+end 
+
 end 
 
 jon_song = Song.new("Hippity Hoppity", "Jon Mendes", "Jon's Way")
 
 puts jon_song.artist
 puts "boy! i sure love that song #{jon_song.name}"
+puts "this is the firt method" 
 puts jon_song.get_count
+puts "the is the second method"
+puts Song.get_count_other_way
